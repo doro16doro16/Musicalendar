ea4296ec6273e9f5fe5d19654455328aa7803dee
doro16doro16 <doro16doro16@gmail.com>
2022-08-30 01:07:48 +0900

    [로고] 로고 제작. 애니메이션 추가

--- components/Alert.jsx
+++ components/Alert.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import styles from "../styles/Home.module.scss";
-import Logo from "../public/Spotify_logo.png";
+import logo_odd from "../public/logo_odd.png";
+import Image from "next/image";
 
 function Alert({ onToggle }) {
   return (
     <div className={styles.modal__outer}>
       <div className={styles.alert}>
-        <img
-          src="Spotify_logo.png"
-          layout="fill" // 상위 요소에 따라 이미지 크기 예약
-          className="animate-pulse"
-        />
-        <p>노래를 먼저 선택해주세요.</p>
-        <button onClick={onToggle}>Close</button>
+        <div>
+          {/* <Image src={logo_odd} alt="" className={styles.logo_left} /> */}
+          <img src="logo_odd.png" className={styles.logo_left} alt="" />
+          <img src="logo_odd.png" className={styles.logo_right} alt="" />
+        </div>
+        <div>
+          <p>노래를 먼저 선택해주세요.</p>
+          <button onClick={onToggle}>Close</button>
+        </div>
       </div>
     </div>
   );
